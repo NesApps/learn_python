e22821a66e431c711e178e35a010842eb5c6fa88
Nestor <freemanchino333@gmail.com>
2026-07-12 07:41:27 +0100

    re-wrote the guessing game and added a new print statement saying 'Thank you for playing

--- guessing_game.py
+++ guessing_game.py
@@ -1,5 +1,5 @@
-secret_number = 7
-
+# secret_number = 7
+#
 # while True:
 #     guess = int(input("Give me your guess: "))
 #
@@ -11,4 +11,33 @@ secret_number = 7
 #
 #     else:
 #         print("Number too low")
+
+
+
+
+secret_number = 7
+
+while True:
+    guess = int(input("Give your guess: "))
+    if guess == secret_number:
+        print("Congratulations, your guess is right")
+
+    elif guess > secret_number:
+        print("Too high")
+
+    else:
+        print("Too low")
+
+    print("Thank you for playing")
+
+
+
+
+
+
+
+
+
+
+
 
