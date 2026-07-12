c2f88c7eec1988d0bf35f57f0a93cc402365ce02
Nestor <freemanchino333@gmail.com>
2026-07-12 09:00:06 +0100

    first commit

---
+++ guesing_game_v3.py
@@ -0,0 +1,20 @@
+import random
+
+secret_number = random.randint(1, 7)
+attempts = 0
+
+while True:
+    guess = int(input("Enter your guess: "))
+    attempts += 1
+    if guess == secret_number:
+        print(f"🎉 Congratulations! You guessed correctly in {attempts} attempts!")
+        break
+
+    elif guess > secret_number:
+        print("📈 Too high! Try again.")
+
+    else:
+        print("📉 Too low! Try again.")
+
+
+print("Thank you for playing")
