aedc4a806bef02abca3ed8638bf6f2c7d8093de5
Nestor <freemanchino333@gmail.com>
2026-07-14 14:53:39 +0100

    practiced with more addition and multoiplication functions

--- function_v1.py
+++ function_v1.py
@@ -1,3 +1,19 @@
-def greet():
-    print("Hello")
-greet()
+# def greet():
+#     print("Hello")
+# greet()
+
+#-----------------------------------------
+# def add(a, b):
+#     return (a + b)
+#
+# result = add(5, 3)
+#
+# print(result)
+
+#------------------------------------------
+def square(number):
+    print(number * number)
+
+x = square(4)
+
+print(x)
