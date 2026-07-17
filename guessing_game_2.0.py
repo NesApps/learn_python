75553cfb701250afb4a2097cf0af1375f6e45eaa
Nestor <freemanchino333@gmail.com>
2026-07-17 06:25:14 +0100

    start guessing_game_2.0

---
+++ guessing_game_2.0.py
@@ -0,0 +1,19 @@
+import random
+
+def get_secret_number():
+    secret_number = random.randint(1, 10)
+    return secret_number
+def get_guess():
+    guess = int(input("Enter your guess: "))
+    return guess
+def check_guess(guess, secret_number):
+    if guess == secret_number:
+        print(f"🎉 Congratulations! You guessed correctly")
+        return True
+    elif guess > secret_number:
+        print(f"📈 Too high! Try again.")
+        return False
+    else:
+        print(f"📈 Too low! Try again.")
+        return False
+attempts = 0
