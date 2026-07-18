542fb7ca90cd60ab192f057ecdb506192fdf48b5
Nestor <freemanchino333@gmail.com>
2026-07-18 22:33:05 +0100

    add another example

--- list.py
+++ list.py
@@ -1,11 +1,18 @@
-countries = [
-    "Nigeria",
-    "Ghana",
-    "Kenya",
-    "Egypt",
-    "South Africa"
-]
-countries.append("Morocco")
-print(countries[-1])
-print(countries[2])
-print(countries)
+# countries = [
+#     "Nigeria",
+#     "Ghana",
+#     "Kenya",
+#     "Egypt",
+#     "South Africa"
+# ]
+# countries.append("Morocco")
+# print(countries[-1])
+# print(countries[2])
+# print(countries)
+
+numbers = [10]
+
+numbers.append(20)
+numbers.append(30)
+
+print(numbers)
