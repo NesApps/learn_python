f96323afa10d49c8462a3df65843fe124769fceb
Nestor <freemanchino333@gmail.com>
2026-07-20 17:23:21 +0100

    combined list, for loop and if statement

--- list.py
+++ list.py
@@ -10,9 +10,15 @@
 # print(countries[2])
 # print(countries)
 
-numbers = [10]
+# numbers = [10]
+#
+# numbers.append(20)
+# numbers.append(30)
+#
+# print(numbers)
 
-numbers.append(20)
-numbers.append(30)
+# scores = [45, 80, 65, 30, 90]
+# for score in scores:
+#     if score >= 60:
+#         print(score)
 
-print(numbers)
