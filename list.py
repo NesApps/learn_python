434b7fea15b7a0f6262279be0728431d2622de0b
Nestor <freemanchino333@gmail.com>
2026-07-22 04:37:47 +0100

    add more for list examples

--- list.py
+++ list.py
@@ -10,26 +10,38 @@
 # print(countries[2])
 # print(countries)
 
+##############################
 # numbers = [10]
-#
 # numbers.append(20)
 # numbers.append(30)
-#
 # print(numbers)
 
+##############################
 # scores = [45, 80, 65, 30, 90]
 # for score in scores:
 #     if score >= 60:
 #         print(score)
 
+##############################
 # numbers = [2, 4, 6, 7, 8]
 #
 # for number in numbers:
 #     if number % 2 == 0:
 #         print(number)
 
-ages = [12, 18, 25, 15]
+##############################
+# ages = [12, 18, 25, 15]
+#
+# for age in ages:
+#     if age >= 18:
+#         print(age)
 
-for age in ages:
-    if age >= 18:
-        print(age)
+##############################
+# numbers = [10, 20, 30, 40]
+#
+# removed = numbers.pop()
+#
+# print(numbers)
+# print(removed)
+
+##############################
