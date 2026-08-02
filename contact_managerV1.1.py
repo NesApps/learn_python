c5a2a9bfb1c141ed72ad662a32c11eddb21b865c
Nestor <freemanchino333@gmail.com>
2026-08-02 16:16:03 +0100

    added else statement for delete contact function

--- contact_managerV1.1.py
+++ contact_managerV1.1.py
@@ -1,28 +1,6 @@
 
 
-######ADD CONTACT LOGIC#####
-# while True:
-#     name = input("Enter contact name: ")
-#     if name == "done":
-#         break
-#     contacts.append(name)
-# print("Your Contacts:")
 
-##ADD VIEW CONTACT LOGIC#####
-# for index, contact in enumerate(contacts):
-#     print(f"{index + 1}. {contact}")
-# search_name = input("Enter contact name: ")
-# found = False
-
-#######SEARCH CONTACT LOGIC########
-# search_name = input("Enter the name you want to search: ")
-# for contact in contacts:
-#     if contact == search_name:
-#         print(f"{search_name} found!")
-#         found = True
-#         break
-# if not found:
-#     print(f"{search_name} not found!")
 
 #######DELETE CONTACT LOGIC########
 # delete_name = input("Enter contact to delete: ")
@@ -72,6 +50,8 @@ def delete_contact():
         contacts.remove(delete_name)
         print(f"{delete_name} successfully deleted")
         print(contacts)
+    else:
+        print(f"{delete_name} not found!")
 
 while True:
     show_menu()
